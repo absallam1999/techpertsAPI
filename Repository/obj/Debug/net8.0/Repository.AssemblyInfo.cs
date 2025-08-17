1418514a150b0f8f64fbcf5826f18a732d636f85
absallam1999 <absallam1999@gmail.com>
2025-08-17 11:58:33 +0300

    Maintenance V1

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba391f21cb5c27cd9f57a26b8525e8f688f91fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6588f56c441b8147ea747ad5120f5b925d4dbcee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
